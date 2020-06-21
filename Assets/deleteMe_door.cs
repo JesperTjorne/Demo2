70c81174cef79a3c72236e981fe2557728996eee
Jesper Tjørnelund <jet@ihfood.dk>
2020-06-21 10:57:51 +0200

    fixed open anim and reversed for closing. test script working

--- Assets/deleteMe_door.cs
+++ Assets/deleteMe_door.cs
@@ -15,7 +15,27 @@ public class deleteMe_door : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space)) 
         {
-            var anim = GetComponent<Animation>().Play();
+            // GetComponent<Animation>().animatePhysics = false;
+            var animation = GetComponent<Animation>();
+
+            animation["Door_Open"].speed = 1;
+            animation["Door_Open"].time = 0;
+
+            animation.Play("Door_Open");
+            // GetComponent<Animation>().Play("Door_Open");
+            // GetComponent<Animator>().SetTrigger("Open");
         }  
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            // GetComponent<Animation>().Play("Door_Close");
+            var animation = GetComponent<Animation>();
+
+            animation["Door_Open"].speed = -1;
+            animation["Door_Open"].time = animation["Door_Open"].length;
+
+            animation.Play("Door_Open");
+            // GetComponent<Animator>().SetTrigger("Close");
+        }
     }
 }
